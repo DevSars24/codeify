26aca24dc0fafd326e93ef89fed977c36f0f7353
saurabh singh <saurabhsingh100605@gmail.com>
2025-12-23 15:17:57 +0530

    fix: add prisma generate to postinstall and update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
 import { defineConfig, env } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
+    // This allows Prisma 7 CLI and migrations to find the URL in Vercel
     url: env("DATABASE_URL"),
   },
 });
